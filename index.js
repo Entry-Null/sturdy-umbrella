12b66666c3e79bdf98c92584454fa1e741f08daf
Entry-Null <72706329+Entry-Null@users.noreply.github.com>
2021-08-23 15:06:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const sock = new js_socket.Server({port:88})
 sock.on("connection",function(client){
     console.log('New Connection')
     client.on('message',function(message){
-        sock.clients.forEach(function each(cli) {
+        sock.clients.forEach(function each(client) {
             if (cli !== sock) {
               client.send(message);
             }
